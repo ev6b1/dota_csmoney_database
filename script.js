093be649f7bbbbf4386a481b4a629a6b2eb4f9a2
ev6b1 <52885799+ev6b1@users.noreply.github.com>
2020-04-05 13:27:41 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ document.querySelector('.search_button').addEventListener('click', () => {
   document.querySelector('.price').innerHTML = '';
   for (var key in csmoney_db) {
     if (csmoney_db[key].m.indexOf(document.querySelector('.search_input').value) != -1) {
-      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${csmoney_db[key].m}, Цена на боте: ${csmoney_db[key].a}$, Цена на депозит: ${csmoney_db[key].a}$</div>`;
+      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${csmoney_db[key].m}, Цена на боте: <strong>${csmoney_db[key].a}$</strong>; Цена на депозит: <strong>${(csmoney_db[key].a * 0.95).toFixed(2)}$</strong>; Выгодно купить на Buff за: <strong>${(csmoney_db[key].a * 0.6 * 7.09).toFixed(0)}￥</strong></div>`;
     }
   }
   for (var key in dota_db) {
     if (dota_db[key].m.indexOf(document.querySelector('.search_input').value) != -1) {
-      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${dota_db[key].m}, Цена на боте: ${dota_db[key].a}$, Цена на депозит: ${dota_db[key].a}$</div>`;
+      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${dota_db[key].m}, Цена на боте: <strong>${dota_db[key].a}$</strong>; Цена на депозит: <strong>${(dota_db[key].a * 0.95).toFixed(2)}$</strong>; Выгодно купить на Buff за: <strong>${(dota_db[key].a * 0.6 * 7.09).toFixed(0)}￥</strong></div>`;
     }
   }
 })
@@ -23,12 +23,12 @@ document.querySelector('.search_input').addEventListener('keydown', (e) => {
   document.querySelector('.price').innerHTML = '';
   for (var key in csmoney_db) {
     if (csmoney_db[key].m.indexOf(document.querySelector('.search_input').value) != -1) {
-      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${csmoney_db[key].m}, Цена на боте: ${csmoney_db[key].a}$, Цена на депозит: ${csmoney_db[key].a}$</div>`;
+      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${csmoney_db[key].m}, Цена на боте: <strong>${csmoney_db[key].a}$</strong>; Цена на депозит: <strong>${(csmoney_db[key].a * 0.95).toFixed(2)}$</strong>; Выгодно купить на Buff за: <strong>${(csmoney_db[key].a * 0.6 * 7.09).toFixed(0)}￥</strong></div>`;
     }
   }
   for (var key in dota_db) {
     if (dota_db[key].m.indexOf(document.querySelector('.search_input').value) != -1) {
-      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${dota_db[key].m}, Цена на боте: ${dota_db[key].a}$, Цена на депозит: ${dota_db[key].a}$</div>`;
+      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${dota_db[key].m}, Цена на боте: <strong>${dota_db[key].a}$</strong>; Цена на депозит: <strong>${(dota_db[key].a * 0.95).toFixed(2)}$</strong>; Выгодно купить на Buff за: <strong>${(dota_db[key].a * 0.6 * 7.09).toFixed(0)}￥</strong></div>`;
     }
   }
 }
