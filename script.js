378a13404741144f9d120540454989935814568e
ev6b1 <52885799+ev6b1@users.noreply.github.com>
2020-07-05 18:49:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.querySelector('.search_button').addEventListener('click', () => {
   document.querySelector('.price').innerHTML = '';
   for (var key in csmoney_db) {
     if (csmoney_db[key].m.indexOf(document.querySelector('.search_input').value) != -1) {
-      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${csmoney_db[key].m}, Цена на боте: <strong>${csmoney_db[key].a}$</strong>; Цена на депозит: <strong>${(csmoney_db[key].a * 0.95).toFixed(2)}$</strong>; Выгодно купить на Buff за: <strong>${(csmoney_db[key].a * 0.6 * 7.09).toFixed(0)}￥</strong></div>`;
+      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${csmoney_db[key].m}, Цена на боте: <strong>${csmoney_db[key].a}$</strong>; Цена на депозит: <strong>${(csmoney_db[key].a * 0.95).toFixed(2)}$</strong>; Выгодно купить на Buff за: <strong>${(csmoney_db[key].a * 0.95 * 0.6 * 7.07).toFixed(0)}￥</strong></div>`;
     }
   }
   for (var key in dota_db) {
@@ -23,7 +23,7 @@ document.querySelector('.search_input').addEventListener('keydown', (e) => {
   document.querySelector('.price').innerHTML = '';
   for (var key in csmoney_db) {
     if (csmoney_db[key].m.indexOf(document.querySelector('.search_input').value) != -1) {
-      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${csmoney_db[key].m}, Цена на боте: <strong>${csmoney_db[key].a}$</strong>; Цена на депозит: <strong>${(csmoney_db[key].a * 0.95).toFixed(2)}$</strong>; Выгодно купить на Buff за: <strong>${(csmoney_db[key].a * 0.6 * 7.09).toFixed(0)}￥</strong></div>`;
+      document.getElementsByClassName('price')[0].innerHTML += `<div class="item">${csmoney_db[key].m}, Цена на боте: <strong>${csmoney_db[key].a}$</strong>; Цена на депозит: <strong>${(csmoney_db[key].a * 0.95).toFixed(2)}$</strong>; Выгодно купить на Buff за: <strong>${(csmoney_db[key].a * 0.95 * 0.6 * 7.07).toFixed(0)}￥</strong></div>`;
     }
   }
   for (var key in dota_db) {
