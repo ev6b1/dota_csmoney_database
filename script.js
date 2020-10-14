e3ea2578b65d5eb325c2cedd2f0a95d57371b151
ev6b1 <52885799+ev6b1@users.noreply.github.com>
2020-10-15 02:51:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var msg = $.ajax({type: "GET", url: "https://cs.money/js/database-skins/library-en-730.js?v=22", async: false}).responseText,
+var msg = $.ajax({type: "GET", url: "https://old.cs.money/js/database-skins/library-en-730.js?v=22", async: false}).responseText,
 csmoney_db = $.parseJSON(msg.replace("skinsBaseList[730] = ",''));
-var secondMsg = $.ajax({type: "GET", url: "https://cs.money/js/database-skins/library-ru-570.js?v=1259", async: false}).responseText,
+var secondMsg = $.ajax({type: "GET", url: "https://old.cs.money/js/database-skins/library-ru-570.js?v=1259", async: false}).responseText,
 dota_db = $.parseJSON(secondMsg.replace("skinsBaseList[570] = ",''));
 
 
